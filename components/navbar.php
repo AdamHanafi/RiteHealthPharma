ababb7b99c7fc0604d673c3051b97f0230e5a6d8
Adam Hanafi <adamhanafi97@gmail.com>
2023-02-26 14:06:05 -0500

    Add files via upload

---
+++ components/navbar.php
@@ -0,0 +1,64 @@
+<!-- Topbar Start -->
+<div class="container-fluid bg-light ps-5 pe-0 d-none d-lg-block">
+    <div class="row gx-0">
+        <div class="col-md-6 text-center text-lg-start mb-2 mb-lg-0">
+            <div class="d-inline-flex align-items-center">
+                <small class="py-2"><i class="far fa-clock text-primary me-2"></i>Opening Hours: Mon - Tues : 9.00 am - 5.00 pm, Sat : 9.00 am - 2.00 pm, Sun Closed </small>
+            </div>
+        </div>
+        <div class="col-md-6 text-center text-lg-end">
+            <div class="position-relative d-inline-flex align-items-center bg-primary text-white top-shape px-5">
+                <div class="me-3 pe-3 border-end py-2">
+                    <p class="m-0"><i class="fa fa-envelope-open me-2"></i>ritehealthpharm@gmail.com</p>
+                </div>
+                <div class="py-2">
+                    <p class="m-0"><i class="fa fa-phone-alt me-2"></i>313-209-6066</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Topbar End -->
+
+
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3 py-lg-0">
+    <a href="index.php" class="navbar-brand p-0">
+        <img class="w-100" src="img/Rite Health Pharmacy Logo.png" alt="Image" style="weidth:200px; height: 190px; padding-bottom: 15px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+</div>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto py-0">
+            <a href="index.php" class="nav-item nav-link active">Home</a>
+            <a href="about.php" class="nav-item nav-link">About</a>
+            <a href="Mobile.php" class="nav-item nav-link">Mobile</a>
+            <a href="service.php" class="nav-item nav-link">Meds info</a>
+            <div class="nav-item dropdown">
+              <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown">Services</a>
+              <div class="dropdown-menu m-0">
+                  <a href="price.php" class="dropdown-item active">Pharmacy</a>
+                    <a href="Medication.php" class="dropdown-item">Medication</a>
+                    <a href="Durable.php" class="dropdown-item">Durable Medical Equipment</a>
+                    <a href="Therapy.php" class="dropdown-item">Medication Therapy Management</a>
+                      <a href="Compounding.php" class="dropdown-item">Compounding</a>
+                          <a href="Medicare.php" class="dropdown-item">Medicare</a>
+                            <a href="Multi-Dose.php" class="dropdown-item">Multi-Dose Packaging</a>
+                            <a href="MS.php" class="dropdown-item">Medication Synchronization</a>
+                            <a href="MS1.php" class="dropdown-item">Immunizations</a>
+                </div>
+            </div>
+              <div class="nav-item dropdown">
+                <a href="#" class="nav-link dropdown-toggle active" data-bs-toggle="dropdown" aria-expanded="false" style=" margin-right: 100px;">Contact</a>
+                <div class="dropdown-menu m-0">
+                    <a href="contact.php" class="dropdown-item">Rite Health Michigan Ave</a>
+                    <a href="contact2.php" class="dropdown-item">Rite Health Vernor</a>
+                      <a href="contact3.php" class="dropdown-item">Rite Health Lansing</a>
+                    <a href="contact4.php" class="dropdown-item">Town Pharmacy</a>
+        </div>
+
+    </div>
+</nav>
+<!-- Navbar End -->
